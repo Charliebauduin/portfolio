ea07c8bbce391ee9e32e3a1c3c0c4ca3192b668a
Charlie <bauduincharlie@gmail.com>
2021-08-17 18:55:37 +0200

    mise en place page accueil et menu fonctionnel

--- Portfolio/src/Controller/HomeController.php
+++ Portfolio/src/Controller/HomeController.php
@@ -11,10 +11,55 @@ class HomeController extends AbstractController
     /**
      * @Route("/", name="home")
      */
-    public function index(): Response
+    public function home(): Response
     {
         return $this->render('home/index.html.twig', [
             'controller_name' => 'HomeController',
         ]);
     }
+
+
+    /**
+     * @Route("/bio", name="bio")
+     */
+    public function bio()
+    {
+        return $this->render('home/bio.html.twig');
+    }
+
+
+    /**
+     * @Route("/resume", name="resume")
+     */
+    public function cv()
+    {
+        return $this->render('home/resume.html.twig');
+    }
+
+    /**
+     * @Route("/skills", name="skills")
+     */
+    public function skills()
+    {
+        return $this->render('home/skills.html.twig');
+    }
+
+    /**
+     * @Route("/realisations", name="realisations")
+     */
+    public function realisations()
+    {
+        return $this->render('home/realisations.html.twig');
+    }
+
+    /**
+     * @Route("/contact", name="contact")
+     */
+    public function contact()
+    {
+        return $this->render('home/contact.html.twig');
+    }
+
 }
+
+
