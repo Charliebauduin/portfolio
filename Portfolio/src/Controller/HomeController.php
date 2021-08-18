ea6a6419cd3b089640ba46d7beaf78ac88733b1b
Charlie <bauduincharlie@gmail.com>
2021-08-18 14:11:42 +0200

    modif sur le css

--- Portfolio/src/Controller/HomeController.php
+++ Portfolio/src/Controller/HomeController.php
@@ -11,7 +11,7 @@ class HomeController extends AbstractController
     /**
      * @Route("/", name="home")
      */
-    public function home(): Response
+    public function home()
     {
         return $this->render('home/index.html.twig', [
             'controller_name' => 'HomeController',
